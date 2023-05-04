8939997cab39fc88313c476e2d83d9be9a7112fd
technologicaljerry <cserajatverma@gmail.com>
2023-05-04 22:27:21 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserRequest } from './user.dto';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly eventEmitter: EventEmitter2) { }
 
   private readonly logger = new Logger(AppService.name);
 
